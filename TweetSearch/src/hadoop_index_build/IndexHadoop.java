2b9890e005ccd4c019d863bebfa329a879fc7f6b
PrajnaShetty <shetty.prajna86@gmail.com>
2014-05-03 11:09:15 -0700

    building index using hadoop

---
+++ TweetSearch/src/hadoop_index_build/IndexHadoop.java
@@ -0,0 +1,77 @@
+package hadoop_index_build;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.filecache.DistributedCache;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.MultipleOutputs;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+
+/**
+ * @author PrajnaS
+ */
+public class IndexHadoop extends Configured implements Tool {
+	
+
+    @Override
+    public int run(String[] args) throws Exception {
+    	Configuration conf = getConf();
+        Job job = new Job(conf, "IndexHadoopNew");
+ 
+        job.setJarByClass(IndexHadoop.class);
+        job.setMapperClass(TweetMapper.class);
+        job.setReducerClass(TweetReducer.class);
+ 
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+        MultipleOutputs.addNamedOutput(job, "text", TextOutputFormat.class,
+        		 Text.class, Text.class);
+        
+        List<String> other_args = new ArrayList<String>();
+	      for (int i=0; i < args.length; ++i) {
+	        if ("-libjars".equals(args[i])) {
+	          DistributedCache.addCacheFile(new Path(args[++i]).toUri(), conf);
+	          conf.setBoolean("indexhadoop.libjars", true);
+	        } else {
+	          other_args.add(args[i]);
+	        }
+	      }
+	
+	      FileInputFormat.setInputPaths(job, new Path(other_args.get(0)));
+	      FileOutputFormat.setOutputPath(job, new Path(other_args.get(1)));
+	      
+      
+        boolean succ = job.waitForCompletion(true);
+        if (! succ) {
+          System.out.println("Job1 failed, exiting");
+          return -1;
+        }
+        
+        return 0;
+        
+    }
+
+    public static void main(String[] args) throws Exception {
+    	
+    	System.out.println("In IndexTweetsHadoop");
+    	
+    	 long startTime = System.currentTimeMillis();
+    	
+        int res = ToolRunner.run(new Configuration(), new IndexHadoop(), args);
+        
+        long endTime = System.currentTimeMillis();
+        System.out.println("Total Time taken for index creation:" + (endTime-startTime));
+        System.exit(res);
+    }
+}
---
+++ TweetSearch/src/lucene/GetQueryServletLucene.java
@@ -0,0 +1,92 @@
+package lucene;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import common.QueryBean;
+
+import lucene.IndexTweetsLucene;
+
+/**
+ * Servlet implementation class GetQueryServlet
+ */
+
+public class GetQueryServletLucene extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public GetQueryServletLucene() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		System.out.println("In Servlet - Lucene");
+		
+		QueryBean query = new QueryBean();
+		Map<String, String> resultsLucene = new HashMap<String, String>();
+		
+		if (request.getParameter("userlocation") != "")
+			query.setUserLocation(request.getParameter("userlocation"));
+		
+		if (request.getParameter("screenname") != "")
+			query.setScreenName(request.getParameter("screenname"));
+		
+		if (request.getParameter("username") != "")
+			query.setUserName(request.getParameter("username"));
+		
+		
+		if (request.getParameter("allwords") != "")
+			query.setAllWords(request.getParameter("allwords"));
+		
+		if (request.getParameter("exactphrase") != "")
+			query.setExactPhrase(request.getParameter("exactphrase"));
+		
+		if (request.getParameter("withoutwords") != "")
+			query.setWithoutWord(request.getParameter("withoutwords"));
+		
+		if (request.getParameter("approxphrase") != "")
+			query.setApproxPhrase(request.getParameter("approxphrase"));
+		
+		if (request.getParameter("hashtags") != "")
+			query.setHashtag(request.getParameter("hashtags"));
+		
+         resultsLucene = new IndexTweetsLucene().getQueryResponse(query);
+		
+        response.setContentType("text/html");
+		PrintWriter writer = response.getWriter();
+		writer.println("<b>LUCENE</b>");
+		writer.println("<html><b>Below is the response to your query!</b></html>");
+		writer.println("Found " + resultsLucene.size() + " hits.<br><br>");
+		int i = 0;
+		for (String result:resultsLucene.values()){
+			writer.println("<br><br>" + (i+1)+ ".  " + result);
+			i++;
+		}
+		writer.println("<br><br><a href=\"Main.jsp\">Back to Home Page</a>");
+		writer.flush();
+		
+	    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request,response);
+	}
+
+}
